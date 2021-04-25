54b78235340b102c6bba2fa1ac535765c56a291d
Simón Oroño <simonorono@protonmail.com>
2021-04-25 18:20:31 -0400

    Stop inlining assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsInlineLimit: 0
+  },
   plugins: [reactRefresh()]
 })
