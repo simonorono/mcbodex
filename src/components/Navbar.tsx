34a87d95befb2ade27d67b63617767190c3d5edf
Simón Oroño <simonorono@protonmail.com>
2021-05-01 22:04:32 -0400

    Fix navbar logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import rDexLogo from '../../media/RDex.png'
+
 export default function Navbar() {
   return (
     <div className="mx-auto px-4 sm:px-6 lg:px-8 border-b mb-5 bg-gray-700">
@@ -8,12 +10,12 @@ export default function Navbar() {
           <div className="flex-shrink-0 flex items-center">
             <img
               className="block lg:hidden h-10 w-auto"
-              src="/media/RDex.png"
+              src={rDexLogo}
               alt="RDex logo"
             />
             <img
               className="hidden lg:block h-10 w-auto"
-              src="/media/RDex.png"
+              src={rDexLogo}
               alt="RDex logo"
             />
           </div>
