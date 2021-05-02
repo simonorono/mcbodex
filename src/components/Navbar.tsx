f7484fc6e2f533d337fa38576dba9b90d9b47ba7
Simón Oroño <simonorono@protonmail.com>
2021-05-02 15:19:11 -0400

    Implement Pokedex selector

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import PokedexSelect from './PokedexSelect'
 
 import rDexLogo from '/media/RDex.png'
 
 export default function Navbar() {
   return (
-    <div className="mx-auto px-4 sm:px-6 lg:px-8 border-b mb-5 bg-gray-700">
+    <div className="mx-auto px-2 sm:px-6 lg:px-8 border-b mb-5 bg-gray-700">
       <div className="flex justify-between h-16">
         <div className="flex">
           <div className="flex-shrink-0 flex items-center">
@@ -19,6 +20,10 @@ export default function Navbar() {
               alt="RDex logo"
             />
           </div>
+        </div>
+
+        <div className="flex items-center">
+          <PokedexSelect></PokedexSelect>
         </div>
       </div>
     </div>
