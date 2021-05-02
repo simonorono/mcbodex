bda32471c11afcbc4f36730b10d64c005d6088fe
Simón Oroño <simonorono@protonmail.com>
2021-05-02 18:29:36 -0400

    Code appearance changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import './index.css'
-
+import App from './App'
 import store from './store'
 
-import App from './App'
+import './index.css'
 
 declare global {
   interface Pokedex {
