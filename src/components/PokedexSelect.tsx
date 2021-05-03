5e3fbd89c8d3c05b808e763408172c3658c1deda
Simón Oroño <simonorono@protonmail.com>
2021-05-03 09:22:37 -0400

    Disable pokedex select while loading

--- src/components/PokedexSelect.tsx
+++ src/components/PokedexSelect.tsx
@@ -11,17 +11,22 @@ function classNames(...classes: Array<String>) {
 export default function PokedexSelect() {
   const allPokedex = useAppSelector(state => state.pokedex.all)
   const currentPokedex = useAppSelector(state => state.pokedex.current)
+  const allPokedexLoaded = useAppSelector(state => state.pokedex.loaded)
 
   const dispatch = useAppDispatch()
 
   const onSelected = (pokedex: Pokedex) => dispatch(setCurrentPokedex(pokedex))
 
   return (
-    <Listbox value={currentPokedex} onChange={onSelected}>
+    <Listbox value={currentPokedex} onChange={onSelected} disabled={!allPokedexLoaded}>
       <>
         <div className="relative w-52 sm:w-60">
-          <Listbox.Button className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-            <span className="block truncate">{currentPokedex?.name}</span>
+          <Listbox.Button className={
+            classNames(
+              allPokedexLoaded ? 'bg-white' : 'bg-gray-200 cursor-not-allowed',
+              "relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            )}>
+            <span className="block truncate">{currentPokedex?.name ?? 'Loading...'}</span>
             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
               <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
             </span>
