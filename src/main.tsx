49e083a62d81582c3032c4a67f99968f6d71d646
Simón Oroño <simonorono@protonmail.com>
2021-05-08 14:35:16 -0400

    Remove register of SW
    It was buggy. Failed to detect changes and invalidate cache.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { registerSW } from 'virtual:pwa-register'
 
 import App from './App'
 import store from './store'
 
 import './index.css'
-
-(registerSW())()
 
 ReactDOM.render(
   <React.StrictMode>
