a371bfda29484d22871cf0b3e1f55e523450af1f
Simón Oroño <simonorono@protonmail.com>
2021-05-08 17:03:16 -0400

    Remove PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import { pwa } from './build/pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +8,5 @@ export default defineConfig({
   },
   plugins: [
     reactRefresh(),
-    pwa(),
   ],
 })
