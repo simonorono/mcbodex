3347552e64b3bc7876e41c2a056f8a41c85e9b85
Simón Oroño <simonorono@protonmail.com>
2021-05-08 13:47:01 -0400

    Remove axios and the graphql functions
    No longer used.

--- src/api/graphql.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const gqlClient = axios.create({
-  baseURL: 'https://beta.pokeapi.co/graphql/v1beta'
-})
-
-export async function execQuery(query: string) {
-  return await gqlClient.post('/', { query })
-}
