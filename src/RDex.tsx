3014e018c88a224ed08f66ce38965c90a82f2977
Simón Oroño <simonorono@protonmail.com>
2021-05-09 09:23:37 -0400

    Add padding on mobile

--- src/RDex.tsx
+++ src/RDex.tsx
@@ -7,7 +7,7 @@ export default function RDex(props: { [key: string]: any }) {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
-      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         {children}
       </div>
     </div >
