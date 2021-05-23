e3e93e927df62c640133b7f4ba8554ba5fef583b
Simón Oroño <simonorono@protonmail.com>
2021-05-22 22:57:05 -0400

    Use tailwind JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   plugins: [],
   purge: ['./index.html', './src/**/*.tsx', './src/**/*.ts'],
