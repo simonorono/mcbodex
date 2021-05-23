c54642816c8dd6bc2606895bd43e84a29dc9e974
Simón Oroño <simonorono@protonmail.com>
2021-05-23 12:45:25 -0400

    Display types in Pokémon list

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useAppSelector } from '../store/hooks'
 import { ImageURL } from '../utils'
+import TypeBadge from './TypeBadge'
 
 interface Props {
   pokemon: PokemonSpecies,
@@ -8,7 +10,10 @@ interface Props {
 
 export default function PokemonCard(props: Props) {
   const pokedexNo = props.number
+
   const pokemon = props.pokemon
+
+  const typesById = useAppSelector(state => state.types.all)
 
   return (
     <div className="w-full flex items-center justify-between p-2 space-x-6 border border-gray-300 rounded-xl">
@@ -19,8 +24,17 @@ export default function PokemonCard(props: Props) {
         alt={`front sprite for ${pokemon.name}`}
       />
       <div className="flex-1 truncate">
-        <div className="flex items-center space-x-3">
+        <div className="flex items-center space-x-3 mb-2">
           <h3 className="text-gray-900 text-sm font-medium truncate">{pokedexNo}. {pokemon.name}</h3>
+        </div>
+
+        <div className="flex">
+          {pokemon.pokemon[0].typeIds.map(typeId => (
+            <TypeBadge
+              key={typeId}
+              type={typesById[typeId]}
+            />
+          ))}
         </div>
       </div>
     </div>
