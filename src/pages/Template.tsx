96729b47f1079fa9435b8886df834004bd9cfc7e
Simón Oroño <simonorono@protonmail.com>
2021-06-13 15:58:24 -0400

    Make h1 bigger on template

--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -10,7 +10,7 @@ export default function Template(props: Props) {
 
   return (
     <div className='flex flex-col'>
-      <h1 className='mb-10 text-3xl font-bold'>
+      <h1 className='mb-10 text-4xl font-bold'>
         {h1}
       </h1>
 
