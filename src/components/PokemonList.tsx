40964d2d083a54ab2b9fdf628ea596f37b779001
Simón Oroño <simonorono@protonmail.com>
2021-06-13 00:32:49 -0400

    Stop using the intersection observer for pagination
    Now the whole page will render in chunks. Using setTimeout with a
    timeout of 0 to avoid blocking the page while rendering big lists.

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppSelector } from '../store/hooks';
 import { scrollToTop } from '../utils';
 import PokemonCard from './PokemonCard'
@@ -36,48 +36,30 @@ export default function PokemonList(props: Props) {
     setPage(1)
   }, [props.pokemonList])
 
-  const loader = useRef(null)
-
-  const onLoadMoreRequested = (entities: { isIntersecting: boolean }[]) => {
-    if (entities[0].isIntersecting) {
-      setPage(page => page + 1)
-    }
-  }
-
   useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: '100px',
-      threshold: 0.5,
+    if (page * PER_PAGE > props.pokemonList.length) {
+      return
     }
 
-    const observer = new IntersectionObserver(onLoadMoreRequested, options)
-
-    if (loader.current) {
-      observer.observe(loader.current!)
-    }
-
-    return () => observer.disconnect()
-  }, [])
+    setTimeout(() => {
+      setPage(page => page + 1)
+    }, 1)
+  }, [page])
 
   return (
-    <>
-      <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {props.pokemonList.slice(0, page * PER_PAGE).map((pokemon: Pokemon, index) => (
-          <li
-            key={pokemon.id}
-            className="col-span-1"
-          >
-            <PokemonCard
-              number={index + 1}
-              pokemon={pokemon}
-              species={pokemonSpeciesById[pokemon.speciesId]}
-            />
-          </li>
-        ))}
-      </ul>
-
-      <div className="h-20" ref={loader}></div>
-    </>
+    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {props.pokemonList.slice(0, page * PER_PAGE).map((pokemon: Pokemon, index) => (
+        <li
+          key={pokemon.id}
+          className="col-span-1"
+        >
+          <PokemonCard
+            number={index + 1}
+            pokemon={pokemon}
+            species={pokemonSpeciesById[pokemon.speciesId]}
+          />
+        </li>
+      ))}
+    </ul>
   )
 }
