fb9eb68c9fc04628008cbe50bd1d8b0c5746fdfe
Simón Oroño <simonorono@protonmail.com>
2021-06-14 18:45:28 -0400

    Generate sitemap.xml during deploy

---
+++ sitemap.mjs
@@ -0,0 +1,55 @@
+import { SitemapStream, streamToPromise } from 'sitemap'
+import { Readable } from 'stream'
+import fs from 'fs'
+
+function readJson(file) {
+  return JSON.parse(
+    fs.readFileSync(file)
+  )
+}
+
+const games = readJson('./data/handcrafted/games.json')
+const types = readJson('./data/raw/types.json')
+
+const now = new Date;
+
+const links = [
+  {
+    url: '/',
+    changefreq: 'weekly',
+    priority: 1.0,
+    lastmod: now
+  },
+  {
+    url: '/about',
+    changefreq: 'weekly',
+    priority: 1.0,
+    lastmod: now
+  },
+]
+
+games.forEach(game => {
+  links.push({
+    url: `/pokedex/${game.code}`,
+    changefreq: 'weekly',
+    priority: 0.9,
+    lastmod: now
+  })
+})
+
+types.forEach(type => {
+  links.push({
+    url: `/type/${type.code}`,
+    changefreq: 'weekly',
+    priority: 0.9,
+    lastmod: now
+  })
+})
+
+const stream = new SitemapStream({
+  hostname: 'https://rdex.mcbodev.com'
+})
+
+const xml = await streamToPromise(Readable.from(links).pipe(stream)).then(data => data.toString())
+
+fs.writeFileSync('./public/sitemap.xml', xml, { flag: 'w+' })
