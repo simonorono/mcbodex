b95970afb5610b2b07806dfd97aec3e8f111890a
Simón Oroño <simonorono@protonmail.com>
2021-07-21 20:53:12 -0400

    Make tab bottom border more noticeable

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -68,7 +68,7 @@ export default function Tabs(props: Props) {
                     value === selected
                       ? 'border-black text-black'
                       : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                    'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer'
+                    'whitespace-nowrap py-4 px-1 border-b-4 font-medium text-sm cursor-pointer'
                   ].join(' ')}
                   onClick={() => setSelected(value)}
                   aria-current={value === selected ? 'page' : undefined}
