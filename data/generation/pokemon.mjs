88ca42d1abb8a8bc7110804c172699caf93cc610
Simón Oroño <simonorono@protonmail.com>
2021-08-21 14:20:31 -0400

    Improve data generation
    Generation scripts now split into several files. Now generating
    Pokémon data files that will be used by the Pokémon page.

---
+++ data/generation/pokemon.mjs
@@ -0,0 +1,123 @@
+import executeQuery from "./query.mjs"
+import fs from "fs"
+
+const speciesQuery = `
+  query pokemonSpecies {
+    species: pokemon_v2_pokemonspecies {
+      id
+      code: name
+      order
+      pokemons: pokemon_v2_pokemons {
+        id
+      }
+      species_name: pokemon_v2_pokemonspeciesnames(where: {pokemon_v2_language: {name: {_eq: "en"}}}) {
+        name
+      }
+      national_dex_number: pokemon_v2_pokemondexnumbers(where: {pokemon_v2_pokedex: {name: {_eq: "national"}}}) {
+        number: pokedex_number
+      }
+    }
+  }
+`
+
+const pokemonQuery = `
+  query pokemons {
+    pokemon: pokemon_v2_pokemon {
+      id
+      code: name
+      types: pokemon_v2_pokemontypes {
+        type: pokemon_v2_type {
+          id
+        }
+      }
+      species: pokemon_v2_pokemonspecy {
+        id
+      }
+    }
+  }
+`
+
+function getPokemonDataQuery(id) {
+  return `
+    query PokemonData {
+      pokemon: pokemon_v2_pokemon(where: {id: {_eq: ${id}}}) {
+        id
+        code: name
+        abilities: pokemon_v2_pokemonabilities {
+          is_hidden
+          ability: pokemon_v2_ability {
+            id
+          }
+        }
+        stats: pokemon_v2_pokemonstats {
+          base_stat
+          effort
+          stat_id
+        }
+      }
+    }
+  `
+}
+
+async function loadSpecies() {
+  const speciesResponse = await executeQuery(speciesQuery)
+
+  const species = speciesResponse.data.species.map(spcy => ({
+    id: spcy.id,
+    code: spcy.code,
+    name: spcy.species_name[0].name,
+    number: spcy.national_dex_number[0].number, // National Pokédex Number
+    pokemonIds: spcy.pokemons.map(pkm => pkm.id).sort((a, b) => a - b)
+  })).sort((a, b) => a.id - b.id)
+
+  fs.writeFileSync('./data/raw/species.json', JSON.stringify(species), { flag: 'w+' })
+}
+
+async function loadPokemon() {
+  const pokemonResponse = await executeQuery(pokemonQuery)
+
+  const pokemons = pokemonResponse.data.pokemon.map(pkm => ({
+    id: pkm.id,
+    code: pkm.code,
+    typeIds: pkm.types.map(type => type.type.id),
+    speciesId: pkm.species.id
+  })).sort((a, b) => a.id - b.id)
+
+  fs.writeFileSync('./data/raw/pokemon.json', JSON.stringify(pokemons), { flag: 'w+' })
+
+  return pokemons
+}
+
+async function loadPokemonData(pkm) {
+  const query = getPokemonDataQuery(pkm.id)
+
+  const response = await executeQuery(query)
+
+  const obj = response.data.pokemon[0]
+
+  const pokemon = {
+    id: obj.id,
+    code: obj.name,
+
+    abilities: obj.abilities.map(ability => ({
+      hidden: ability.is_hidden,
+      id: ability.ability.id
+    })).sort((a1, a2) => a1.id - a2.id),
+
+    stats: obj.stats.map(stat => ({
+      id: stat.stat_id,
+      base: stat.base_stat,
+      effort: stat.effort,
+    })).sort((s1, s2) => s1.id - s2.id)
+  }
+
+  fs.writeFileSync(`./data/raw/pokemon/${pkm.id}.json`, JSON.stringify(pokemon), { flag: 'w+' })
+}
+
+export default async function load() {
+  await loadSpecies()
+
+  const pokemons = await loadPokemon()
+
+  pokemons.forEach(loadPokemonData)
+}
