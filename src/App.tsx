e462d57d1213b11a03157749d52d5e45a2a88d5a
Simón Oroño <simonorono@protonmail.com>
2021-11-13 18:10:24 -0400

    Update dependencies and adapt for react-router-dom version 6
    As part of upgrading to react-router-dom 6, the ScrollToTop component
    that listened to the history to scroll back up was removed; it was
    never needed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import BackToTopButton from './components/BackToTopButton'
 import Loader from "./components/Loader"
@@ -9,16 +9,6 @@ import routes from './routes'
 import { loadAllPokemon, loadPokedexList } from './store'
 import { useAppDispatch, useAppSelector } from './store/hooks'
 import { classNames } from "./utils"
-
-function scrollToTopInner(props: any) {
-  const { history } = props
-
-  useEffect(() => history.listen(() => window.scrollTo(0, 0)))
-
-  return null
-}
-
-const ScrollToTop = withRouter(scrollToTopInner)
 
 function App() {
   const dispatch = useAppDispatch()
@@ -42,11 +32,15 @@ function App() {
 
         <main className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 w-full">
           <Suspense fallback={<Loader />}>
-            <Switch>
-              {routes.map((route, i) => (
-                <Route key={i} {...route} />
-              ))}
-            </Switch>
+            <Routes>
+              {routes.map((route, i) => {
+                const Component = route.component
+
+                return (
+                  <Route key={i} path={route.path} element={<Component />} />
+                )
+              })}
+            </Routes>
           </Suspense>
         </main>
 
@@ -54,8 +48,6 @@ function App() {
           <div className="h-20" />
         </footer>
       </div>
-
-      <ScrollToTop />
 
       <BackToTopButton />
     </Router>
