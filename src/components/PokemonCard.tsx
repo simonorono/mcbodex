9eac19c47275ff6e07a284caa59a3d4fca6eddb4
Simón Oroño <simonorono@protonmail.com>
2021-11-28 16:11:01 -0400

    Make the whole Pokémon card a link
    Works better on mobile screens.

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -14,29 +14,29 @@ export default function PokemonCard({ pokemon, number }: Props) {
   const species = useAppSelector(state => state.pokemon.speciesById[pokemon.speciesId])
 
   return (
-    <div className="w-full flex items-center p-2 border border-gray-300 rounded-xl">
-      <LazyImage
-        width={80} height={80}
-        className="w-20 h-20 bg-gray-300 rounded-full flex-shrink-0"
-        src={images.frontSpriteForPokemonId(pokemon.id)}
-        alt={`front sprite for ${pokemon.name}`}
-      />
-      <div className="ml-6 flex-grow truncate">
-        <div className="flex flex-col mb-2">
-          <Link to={`/species/${species.code}`} className="hover:underline self-start">
+    <Link to={`/species/${species.code}`} className="hover:underline">
+      <div className="w-full flex items-center p-2 border border-gray-300 rounded-xl">
+        <LazyImage
+          width={80} height={80}
+          className="w-20 h-20 bg-gray-300 rounded-full flex-shrink-0"
+          src={images.frontSpriteForPokemonId(pokemon.id)}
+          alt={`front sprite for ${pokemon.name}`}
+        />
+        <div className="ml-6 flex-grow truncate">
+          <div className="flex flex-col mb-2">
             <h3 className="text-gray-900 text-sm font-medium truncate">
               {number}. {species.name}
             </h3>
-          </Link>
-          {(species.name !== pokemon.name) && (
-            <h4 className="text-gray-700 text-sm truncate">{pokemon.name}</h4>
-          )}
-        </div>
+            {(species.name !== pokemon.name) && (
+              <h4 className="text-gray-700 text-sm truncate">{pokemon.name}</h4>
+            )}
+          </div>
 
-        <div className="flex space-x-1">
-          <PokemonTypes typesRels={pokemon.types} />
+          <div className="flex space-x-1">
+            <PokemonTypes typesRels={pokemon.types} />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
