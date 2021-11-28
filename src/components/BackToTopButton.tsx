eb6296de5d1043ef12baddd3d067aadd429583b7
Simón Oroño <simonorono@protonmail.com>
2021-11-28 11:26:35 -0400

    Apply tabindex -1 to BackToTop button when it's hidden
    To prevent assistive technologies users to be able to focus it when using
    keyboard navigation.

--- src/components/BackToTopButton.tsx
+++ src/components/BackToTopButton.tsx
@@ -3,11 +3,11 @@ import { ArrowUpIcon } from '@heroicons/react/solid'
 import { scrollToTop } from '../utils'
 
 export default function BackToTopButton() {
-  const [show, setShow] = useState(false)
+  const [shown, setIfShown] = useState(false)
 
   useEffect(() => {
     function onWindowScroll() {
-      setShow(() => document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+      setIfShown(() => document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
     }
 
     window.addEventListener('scroll', onWindowScroll)
@@ -22,10 +22,11 @@ export default function BackToTopButton() {
         'inline-flex items-center p-3 border border-transparent rounded-full',
         'shadow-sm text-white bg-primary-700 hover:bg-primary-800 fixed',
         'transition-all duration-500 bottom-6',
-        show ? 'right-6' : '-right-20',
+        shown ? 'right-6' : '-right-20',
       ].join(' ')}
       onClick={scrollToTop}
-      aria-hidden={!show}
+      aria-hidden={!shown}
+      tabIndex={shown ? undefined : -1}
     >
       <span className="sr-only">Back to top</span>
       <ArrowUpIcon className="h-6 w-6" aria-hidden="true" />
