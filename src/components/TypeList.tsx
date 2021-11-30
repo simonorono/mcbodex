be0dd5635ccb00f7330778da5ee82ee78adc5d93
Simón Oroño <simonorono@protonmail.com>
2021-11-30 08:09:25 -0400

    Make the type list a grid
    It looks nicer than the flex.

--- src/components/TypeList.tsx
+++ src/components/TypeList.tsx
@@ -4,23 +4,14 @@ import { types } from '../utils'
 
 export default function TypeList() {
   return (
-    <div className="p-10 border border-gray-300 rounded-md">
+    <div className="p-10 border border-gray-300 rounded-md space-y-4">
       <h2 className="text-2xl">Pokémon types</h2>
 
-      <div
-        className={[
-          'flex flex-wrap justify-center space-x-2 space-y-4',
-          'text-2xl'
-        ].join(' ')}
-      >
-        {types.all.map((type, i) => (
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 gap-2 text-2xl">
+        {types.all.map(type => (
           <TypeBadge
             key={type.id}
             type={type}
-            className={[
-              `hover:underline ${i === 0 ? 'mt-4' : ''}`,
-              'py-0.5 px-4',
-            ].join(' ')}
           />
         ))}
       </div>
