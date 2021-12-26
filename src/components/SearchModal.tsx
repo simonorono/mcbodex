a13489bae944c29edf3b7e9d30a15f8be23c949e
Simón Oroño <simonorono@protonmail.com>
2021-12-26 18:54:47 -0400

    Simplify transition of search modal

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -46,60 +46,50 @@ export default function SearchModal({ close, open }: SearchModalProperties) {
   }
 
   return (
-    <Transition.Root show={open}>
-      <Dialog as="div" static className="fixed inset-0 overflow-hidden" open={open} onClose={close}>
+    <Transition
+      show={open}
+      as={Fragment}
+      enter="transition-opacity duration-200"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-200"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <Dialog as="div" static className="fixed inset-0 overflow-hidden" onClose={close}>
         <div className="absolute inset-0 overflow-hidden">
-          <Transition.Child
-            enter="ease-in-out duration-500"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in-out duration-500"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-          </Transition.Child>
+          <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
 
           <div className="w-[85%] sm:w-96 mx-auto rounded mt-[10%] sm:mt-[5%] overflow-hidden relative z-10">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-in-out duration-500"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in-out duration-500"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div>
-                <input
-                  ref={input}
-                  placeholder="Enter at least 3 characters"
-                  className="w-full text-lg py-2 px-2 border-b border-gray-400 focus:outline-none"
-                  onClick={ev => ev.stopPropagation()}
-                  value={query}
-                  onChange={ev => updateQuery(ev.target.value)}
-                />
+            <div>
+              <input
+                ref={input}
+                placeholder="Enter at least 3 characters"
+                className="w-full text-lg py-2 px-2 border-b border-gray-400 focus:outline-none"
+                onClick={ev => ev.stopPropagation()}
+                value={query}
+                onChange={ev => updateQuery(ev.target.value)}
+              />
 
-                {results && results.map(specy => (
-                  <Link
-                    key={specy.id}
-                    to={`/species/${specy.code}`}
-                    className={[
-                      "p-4 bg-white flex justify-between",
-                      "border-b border-gray-200",
-                      "hover:bg-primary-700 hover:text-white"
-                    ].join(' ')}
-                    onClick={onResultClicked}
-                  >
-                    <span className="font-medium">{specy.name}</span>
-                    <span className="text-md font-light">Pokémon</span>
-                  </Link>
-                ))}
-              </div>
-            </Transition.Child>
+              {results && results.map(specy => (
+                <Link
+                  key={specy.id}
+                  to={`/species/${specy.code}`}
+                  className={[
+                    "p-4 bg-white flex justify-between",
+                    "border-b border-gray-200",
+                    "hover:bg-primary-700 hover:text-white"
+                  ].join(' ')}
+                  onClick={onResultClicked}
+                >
+                  <span className="font-medium">{specy.name}</span>
+                  <span className="text-md font-light">Pokémon</span>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </Dialog>
-    </Transition.Root>
+    </Transition>
   )
 }
