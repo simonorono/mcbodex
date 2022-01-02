559a120e46fdf3434c9d618366e22e8698637226
Simón Oroño <simonorono@protonmail.com>
2022-01-02 17:15:39 -0400

    Switch from @vitejs/plugin-react-refresh to @vitejs/plugin-react
    In order to have HMR.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +7,6 @@ export default defineConfig({
     assetsInlineLimit: 0
   },
   plugins: [
-    reactRefresh(),
+    react(),
   ],
 })
