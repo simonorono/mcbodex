c3234280926cc9e6c3fa34bfed8a64a2d048f3db
Simón Oroño <simonorono@protonmail.com>
2022-02-26 20:50:39 -0400

    Add text for screen readers in table

--- src/components/pokemon_data/BaseStats.tsx
+++ src/components/pokemon_data/BaseStats.tsx
@@ -24,29 +24,35 @@ export default function BaseStats({ className, pokemonData }: BaseStatsProps) {
         <tr className="text-left">
           <th>Stat</th>
           <th className="pl-3">Base</th>
-          <th />
+          <th><span className="sr-only">Percentage</span></th>
           <th className="pl-2">Min</th>
           <th className="pl-2">Max</th>
         </tr>
         </thead>
         <tbody>
-        {pokemonData.stats.map(pkmStat => (
-          <tr key={pkmStat.id}>
-            <td className="font-medium">{stats.byId[pkmStat.id].name}</td>
-            <td className="pl-3">{pkmStat.base}</td>
-            <td className="w-full pl-3">
-              <div className="w-full bg-neutral-200 overflow-hidden rounded-full">
-                <div
-                  data-stat-level={Math.min(6, Math.ceil(pkmStat.base / DATA_LEVEL_DIVISOR))}
-                  className="h-4 rounded-full"
-                  style={{ width: `${(pkmStat.base / 255) * 100}%` }}
-                />
-              </div>
-            </td>
-            <td className="pl-2">{(pkmStat.id === 1 ? stats.minHp : stats.minStat)(pkmStat.base)}</td>
-            <td className="pl-2">{(pkmStat.id === 1 ? stats.maxHp : stats.maxStat)(pkmStat.base)}</td>
-          </tr>
-        ))}
+        {pokemonData.stats.map(pkmStat => {
+          const percentage = ((pkmStat.base / 255) * 100).toFixed(2);
+
+          return (
+            <tr key={pkmStat.id}>
+              <td className="font-medium">{stats.byId[pkmStat.id].name}</td>
+              <td className="pl-3">{pkmStat.base}</td>
+              <td className="w-full pl-3">
+                <div className="w-full bg-neutral-200 overflow-hidden rounded-full">
+                  <div
+                    data-stat-level={Math.min(6, Math.ceil(pkmStat.base / DATA_LEVEL_DIVISOR))}
+                    className="h-4 rounded-full"
+                    style={{ width: `${percentage}%` }}
+                  >
+                    <span className="sr-only">{percentage}</span>
+                  </div>
+                </div>
+              </td>
+              <td className="pl-2">{(pkmStat.id === 1 ? stats.minHp : stats.minStat)(pkmStat.base)}</td>
+              <td className="pl-2">{(pkmStat.id === 1 ? stats.maxHp : stats.maxStat)(pkmStat.base)}</td>
+            </tr>
+          )
+        })}
         <tr>
           <td className="font-medium">Total</td>
           <td className="font-medium pl-3">{sum}</td>
