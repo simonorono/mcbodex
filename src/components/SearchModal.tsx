bbd0728e30a42ad2b467a09019367d06935d765c
Simón Oroño <simonorono@protonmail.com>
2022-03-19 17:16:17 -0400

    Rewrite SearchModal
    It's now using Combobox from HeadlessUI in favour of the not-so-good
    accessibility-wise custom solution.
    
    Improved the code, so it's easier to add a new kind of result, i.e.,
    abilities.

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -1,101 +1,196 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Combobox, Dialog, Transition } from '@headlessui/react'
+import { EmojiSadIcon, SearchIcon, ClockIcon } from '@heroicons/react/outline'
 import { useAppSelector } from '../store/hooks'
-import { Link } from 'react-router-dom'
-import { Dialog, Transition } from '@headlessui/react'
 
 interface Props {
   close: () => void
   open: boolean
 }
 
+interface SearchResult {
+  name: string
+  path: string
+}
+
+interface Results {
+  species: SearchResult[]
+}
+
+const SEARCH_MESSAGE_ICON_ATTRIBUTES = {
+  className: 'mx-auto h-6 w-6 text-gray-400',
+  'aria-hidden': true,
+}
+
+const EMPTY_RESULTS: Results = { species: [] }
+
+function searchMessage(
+  icon: React.ReactNode,
+  mainMessage: string,
+  subMessage?: string
+) {
+  return (
+    <div className="border-t border-gray-100 py-14 px-6 text-center text-sm sm:px-14">
+      {icon}
+      <p className="mt-4 font-semibold text-gray-900">{mainMessage}</p>
+      {subMessage && (
+        <p className="mt-2 text-gray-500">
+          We couldn't find anything with that term. Please try again.
+        </p>
+      )}
+    </div>
+  )
+}
+
+const NOT_FOUND_MESSAGE = searchMessage(
+  <EmojiSadIcon {...SEARCH_MESSAGE_ICON_ATTRIBUTES} />,
+  'No results found',
+  "We couldn't find anything with that term. Please try again."
+)
+
+const LOADING_MESSAGE = searchMessage(
+  <ClockIcon {...SEARCH_MESSAGE_ICON_ATTRIBUTES} />,
+  'Searching...'
+)
+
 export default function SearchModal({ close, open }: Props) {
   const [query, setQuery] = useState('')
-  const [results, setResults] = useState([] as PokemonSpecies[])
+  const [results, setResults] = useState(EMPTY_RESULTS)
+  const [loading, setIfLoading] = useState(false)
   const [timeoutId, setTimeoutId] = useState(0)
-  const input = useRef<HTMLInputElement>(null)
-  const species = useAppSelector(state => state.pokemon.allSpecies)
 
-  useEffect(() => {
-    input.current?.focus()
-  })
+  const species = useAppSelector(state => state.pokemon.allSpecies)
+  const navigate = useNavigate()
+
+  const totalResults = results.species.length
+  const notFound = query.length >= 3 && !loading && totalResults === 0
+  const searching = query.length >= 3 && loading
 
   const updateQuery = (value: string) => {
     setQuery(value)
 
+    // not enough characters; not loading and empty results
     if (value.length < 3) {
-      setResults([])
+      setIfLoading(false)
+      setResults(EMPTY_RESULTS)
       return
     }
 
+    // starts loading
+    setIfLoading(true)
+
+    // stop current search if ongoing
     clearTimeout(timeoutId)
 
+    // start new search
     setTimeoutId(
       setTimeout(() => {
-        setResults(
-          species
-            .filter(spcy =>
-              spcy.name.toLowerCase().includes(value.toLowerCase())
-            )
-            .slice(0, 10)
-        )
+        setResults({
+          species: species
+            .filter(s => s.name.toLowerCase().includes(value.toLowerCase()))
+            .map(s => ({ name: s.name, path: `species/${s.code}` })),
+        })
+
+        // after setting the results, no more loading
+        setIfLoading(false)
       })
     )
   }
 
-  const onResultClicked = () => {
+  // Clears the query & results whenever the combobox shows/hides.
+  useEffect(() => {
+    updateQuery('')
+  }, [open])
+
+  const onResultClicked = (result: SearchResult) => {
     close()
     updateQuery('')
+    navigate(result.path)
   }
 
   return (
-    <Transition
-      show={open}
-      as={Fragment}
-      enter="transition-opacity duration-200"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="transition-opacity duration-200"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-    >
+    <Transition.Root show={open} as={Fragment} afterLeave={() => setQuery('')}>
       <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20"
         onClose={close}
-        className="fixed inset-0 z-10 flex justify-center"
       >
-        <Dialog.Overlay className="fixed inset-0 bg-black opacity-60" />
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Overlay className="absolute inset-0 bg-black bg-opacity-60" />
+        </Transition.Child>
 
-        <div className="relative overflow-hidden">
-          <div className="relative top-[5%] overflow-hidden rounded sm:top-[10%] sm:w-96">
-            <div>
-              <input
-                ref={input}
-                placeholder="Enter at least 3 characters"
-                className="w-full border-b border-gray-400 py-2 px-2 text-lg focus:outline-none"
-                onClick={ev => ev.stopPropagation()}
-                value={query}
-                onChange={ev => updateQuery(ev.target.value)}
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Combobox
+            as="div"
+            className="mx-auto max-w-xl transform overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all"
+            onChange={(result: SearchResult) => onResultClicked(result)}
+            value={null}
+          >
+            <div className="relative">
+              <SearchIcon
+                className="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400"
+                aria-hidden="true"
               />
-
-              {results &&
-                results.map(specy => (
-                  <Link
-                    key={specy.id}
-                    to={`/species/${specy.code}`}
-                    className={[
-                      'flex justify-between bg-white p-4',
-                      'border-b border-gray-200',
-                      'hover:bg-primary-700 hover:text-white',
-                    ].join(' ')}
-                    onClick={onResultClicked}
-                  >
-                    <span className="font-medium">{specy.name}</span>
-                    <span className="text-md font-light">Pokémon</span>
-                  </Link>
-                ))}
+              <Combobox.Input
+                className="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm"
+                placeholder="Write three or more characters..."
+                onChange={event => updateQuery(event.target.value)}
+              />
             </div>
-          </div>
-        </div>
+
+            {totalResults > 0 && (
+              <Combobox.Options
+                static
+                className="max-h-80 scroll-pt-11 scroll-pb-2 space-y-2 overflow-y-auto pb-2"
+              >
+                {results.species.length > 0 && (
+                  <li>
+                    <h2 className="bg-gray-100 py-2.5 px-4 text-xs font-semibold text-gray-900">
+                      Pokémon
+                    </h2>
+                    <ul className="mt-2 text-sm text-gray-800">
+                      {results.species.map(result => (
+                        <Combobox.Option
+                          key={result.path}
+                          value={result}
+                          className={({ active }) =>
+                            [
+                              'cursor-default select-none px-4 py-2',
+                              active && 'bg-primary-600 font-medium text-white',
+                            ].join(' ')
+                          }
+                        >
+                          {result.name}
+                        </Combobox.Option>
+                      ))}
+                    </ul>
+                  </li>
+                )}
+              </Combobox.Options>
+            )}
+
+            {notFound && NOT_FOUND_MESSAGE}
+            {searching && LOADING_MESSAGE}
+          </Combobox>
+        </Transition.Child>
       </Dialog>
-    </Transition>
+    </Transition.Root>
   )
 }
