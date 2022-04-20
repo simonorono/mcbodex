c33f5d31da48abb7fae9e144a19f4b60d2255c02
Simón Oroño <simonorono@protonmail.com>
2022-04-20 08:00:25 -0400

    Improve type effectiveness display on type page

--- src/pages/Type.tsx
+++ src/pages/Type.tsx
@@ -1,11 +1,21 @@
-import React, { ReactElement, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { frontPokemonOfSpeciesByPredicate } from '../store/selectors'
-import TypeBadge from '../components/TypeBadge'
 import PokemonList from '../components/PokemonList'
 import Loader from '../components/Loader'
+import TypeDamageBadge from '../components/TypeDamageBadge'
 import { useAppSelector } from '../store/hooks'
 import { title, types } from '../utils'
+
+function renderEffectiveness(data: { [id: number]: number }) {
+  return (
+    <div className="grid grid-cols-3 gap-2 sm:grid-cols-2 lg:grid-cols-3">
+      {types.all.map(type => (
+        <TypeDamageBadge key={type.id} type={type} effect={data[type.id]} />
+      ))}
+    </div>
+  )
+}
 
 export default function Type() {
   useEffect(() => {
@@ -26,6 +36,10 @@ export default function Type() {
     return pkm.types.map(_ => _.typeId).includes(type.id)
   })
 
+  const attackEffectiveness = types.attackEffectiveness(type)
+
+  const defenseEffectiveness = types.defenseEffectiveness([type])
+
   return (
     <>
       {pokemonLoaded && !type && <p>Not found.</p>}
@@ -36,14 +50,36 @@ export default function Type() {
         <>
           <h1 className="page-title">{`${type.name} Type`}</h1>
 
-          <h2 className="mb-2 text-2xl font-medium">
-            Pokémon with {type.name} type
-          </h2>
+          <div className="space-y-10">
+            <div className="flex flex-col space-y-10 sm:flex-row sm:space-y-0 sm:space-x-10 md:space-x-32">
+              <div className="flex-1">
+                <h2 className="mb-2 text-xl font-medium">
+                  Attack effectiveness
+                </h2>
 
-          <PokemonList
-            numberCallback={pokemon => pokemon.speciesId}
-            pokemonList={pokemonList}
-          />
+                {renderEffectiveness(attackEffectiveness)}
+              </div>
+
+              <div className="flex-1">
+                <h2 className="mb-2 text-xl font-medium">
+                  Defense effectiveness
+                </h2>
+
+                {renderEffectiveness(defenseEffectiveness)}
+              </div>
+            </div>
+
+            <div>
+              <h2 className="mb-2 text-2xl font-medium">
+                Pokémon with {type.name} type
+              </h2>
+
+              <PokemonList
+                numberCallback={pokemon => pokemon.speciesId}
+                pokemonList={pokemonList}
+              />
+            </div>
+          </div>
         </>
       )}
     </>
