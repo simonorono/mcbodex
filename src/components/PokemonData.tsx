dcfaa5eb65605890d13b2650015e169ec8f558d4
Simón Oroño <simonorono@protonmail.com>
2022-08-06 15:43:28 -0400

    Add placeholder text if image can't load

--- src/components/PokemonData.tsx
+++ src/components/PokemonData.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { images } from '../utils'
 import Loader from './Loader'
 import BaseStats from './pokemon_data/BaseStats'
@@ -24,6 +24,9 @@ export default function PokemonData({ pokemon }: Props) {
   const [isLoading, setIsLoading] = useState(true)
   const [pokemonData, setPokemonData] = useState(null as PokemonData | null)
 
+  const imgContainerRef = useRef<HTMLDivElement>(null)
+  const imgErrorRef = useRef<HTMLParagraphElement>(null)
+
   const dataFile = pokemons[key(String(pokemon.id))]
 
   if (dataFile) {
@@ -37,6 +40,18 @@ export default function PokemonData({ pokemon }: Props) {
     currentTarget.classList.remove('opacity-0')
   }
 
+  const onImageError = ({ currentTarget }: React.SyntheticEvent) => {
+    currentTarget.classList.add('hidden')
+
+    if (imgContainerRef.current) {
+      imgContainerRef.current.classList.add('hidden')
+    }
+
+    if (imgErrorRef.current) {
+      imgErrorRef.current.classList.remove('hidden')
+    }
+  }
+
   return (
     <>
       {isLoading && <Loader />}
@@ -45,17 +60,27 @@ export default function PokemonData({ pokemon }: Props) {
         <div className="mx-auto max-w-4xl space-y-6">
           <div className="flex flex-col items-center md:flex-row md:items-start">
             <div
-              className="min-w-[90%] border bg-gray-50 sm:min-w-[450px]"
+              className="flex min-w-[90%] items-center justify-center border bg-gray-50 sm:min-w-[450px]"
               style={imageContainerStyle}
             >
-              <img
-                width={MAX_IMAGE_DIMENSION}
-                height={MAX_IMAGE_DIMENSION}
-                src={images.dataPageImage(pokemon.id)}
-                alt={`artwork for ${pokemon.name}`}
-                className="h-full w-full opacity-0 transition-opacity sm:min-w-[450px]"
-                onLoad={onImageLoad}
-              />
+              <div ref={imgContainerRef} style={imageContainerStyle}>
+                <img
+                  width={MAX_IMAGE_DIMENSION}
+                  height={MAX_IMAGE_DIMENSION}
+                  src={images.dataPageImage(pokemon.id)}
+                  alt={`artwork for ${pokemon.name}`}
+                  className="absolute top-0 h-full w-full opacity-0 transition-opacity"
+                  onLoad={onImageLoad}
+                  onError={onImageError}
+                />
+              </div>
+
+              <p
+                ref={imgErrorRef}
+                className="hidden text-center text-2xl font-medium"
+              >
+                Couldn't load image.
+              </p>
             </div>
 
             <div className="mt-10 flex w-full grow flex-col justify-center px-2 sm:px-10 md:mt-0">
