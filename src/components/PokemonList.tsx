a214d37841fe1d0f3403d64f84cff40525723314
Simón Oroño <simonorono@protonmail.com>
2022-10-21 19:48:05 -0400

    Implement intersection observer to gradually load Pokemon lists
    In order to improve performance.

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import Loader from './Loader'
 import PokemonCard from './PokemonCard'
 
@@ -7,8 +8,21 @@ interface Props {
   numberCallback?: (pokemon: Pokemon) => number
 }
 
+const PER_PAGE = 60
+
 export default function PokemonList({ pokemonList, numberCallback }: Props) {
   const [loaded, setLoaded] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const allPagesLoaded = currentPage * PER_PAGE > pokemonList.length
+
+  const { ref: scrollDetector } = useInView({
+    onChange: inView => {
+      if (inView && !allPagesLoaded) {
+        setCurrentPage(currentPage + 1)
+      }
+    },
+  })
 
   useEffect(() => setLoaded(true), [])
 
@@ -17,21 +31,27 @@ export default function PokemonList({ pokemonList, numberCallback }: Props) {
       {!loaded && <Loader />}
 
       {loaded && (
-        <ul
-          className={[
-            'grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3',
-            loaded ? '' : 'hidden',
-          ].join(' ')}
-        >
-          {pokemonList.map((pokemon: Pokemon, index) => (
-            <li key={pokemon.id} className="col-span-1">
-              <PokemonCard
-                number={numberCallback ? numberCallback(pokemon) : index + 1}
-                pokemon={pokemon}
-              />
-            </li>
-          ))}
-        </ul>
+        <>
+          <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            {pokemonList
+              .slice(0, currentPage * PER_PAGE)
+              .map((pokemon: Pokemon, index) => (
+                <li key={pokemon.id} className="col-span-1">
+                  <PokemonCard
+                    number={
+                      numberCallback ? numberCallback(pokemon) : index + 1
+                    }
+                    pokemon={pokemon}
+                  />
+                </li>
+              ))}
+          </ul>
+
+          <div
+            ref={scrollDetector}
+            className="relative -top-96 -z-50 border-2 border-black"
+          />
+        </>
       )}
     </>
   )
