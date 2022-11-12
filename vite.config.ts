d063ff32a2916b34ccb7e05b0adafc0dd2a2e4e0
Simón Oroño <simonorono@protonmail.com>
2022-11-12 07:33:13 -0400

    Add legacy plugin for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    assetsInlineLimit: 0
+    assetsInlineLimit: 0,
   },
   plugins: [
     react(),
+    legacy(),
   ],
 })
