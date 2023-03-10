8acde47e36e5e7a37571b27a4ae2cb0ca67f2193
Simón Oroño <simonorono@protonmail.com>
2023-03-10 07:47:10 -0400

    Use different base URL for the official artwork while waiting PR merge

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,9 +1,18 @@
 const POKEAPI_BASE_SPRITE_URL =
   'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon'
 
+const TEMP_BASE_SPRICE_URL =
+  'https://raw.githubusercontent.com/simonorono/sprites/pokemon_day_artwork/sprites/pokemon'
+
 export default {
   artworkForPokemon(id: number) {
-    return `${POKEAPI_BASE_SPRITE_URL}/other/official-artwork/${id}.png`
+    /**
+     * Revert once the artwork for Pokémon 1009 & 1010 has been merged.
+     *
+     * See: https://github.com/PokeAPI/sprites/pull/109
+     */
+    // TODO: revert to use PokeAPI URL once PR
+    return `${TEMP_BASE_SPRICE_URL}/other/official-artwork/${id}.png`
   },
 
   homeImageForPokemon(id: number) {
