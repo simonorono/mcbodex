742c7fc30e6ab91f9998c9bb7b4ff5a18d75523e
Simón Oroño <simonorono@protonmail.com>
2023-03-10 22:17:56 -0400

    Add Pokédex filter to search by EV yield

--- src/pages/SearchByEVYield.tsx
+++ src/pages/SearchByEVYield.tsx
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from 'react'
-import { stats, title } from '../utils'
-import Select from '../components/Select'
-import { frontPokemonOfSpeciesByPredicate } from '../store/selectors'
+import { useAppSelector } from '../store/hooks'
+import {
+  frontPokemonOfSpeciesByPredicate,
+  getPokemonListForPokedex,
+} from '../store/selectors'
 import PokemonList from '../components/PokemonList'
+import Select from '../components/Select'
+import { stats, title } from '../utils'
 
 function statToOption(stat: Stat): SelectOption {
   return {
     label: stat.name,
     value: String(stat.id),
+  }
+}
+
+function pokedexToOption(pokedex: Pokedex): SelectOption {
+  return {
+    label: pokedex.name,
+    value: pokedex.code,
   }
 }
 
@@ -23,17 +34,35 @@ export default function SearchByEVYield() {
     document.title = title('Search Pokémon By EV Yield')
   })
 
+  const allPokedex = useAppSelector(state => state.pokedex.allPokedex)
+  const pokedexByCode = useAppSelector(state => state.pokedex.pokedexByCode)
+
   const [stat, setStat] = useState(null as Stat | null)
   const statOptions = stats.all.map(statToOption)
   const selectedStat = stat ? statToOption(stat) : null
 
-  const pokemonList = frontPokemonOfSpeciesByPredicate(pkm => {
+  const [pokedex, setPokedex] = useState(null as Pokedex | null)
+  const pokedexOptions = allPokedex.map(pokedexToOption)
+  const selectedPokedex = pokedex ? pokedexToOption(pokedex) : null
+
+  const allPokemonWithYield = frontPokemonOfSpeciesByPredicate(pkm => {
     if (!stat) {
+      return false
+    }
+
+    // if a Pokédex is selected, we don't care about this, just return false
+    if (pokedex) {
       return false
     }
 
     return pkm.evYield.includes(stat.id)
   })
+
+  const pokedexPokemonWithYield = getPokemonListForPokedex(pokedex).filter(
+    pkm => stat && pkm.evYield.includes(stat.id)
+  )
+
+  const pokemonList = pokedex ? pokedexPokemonWithYield : allPokemonWithYield
 
   return (
     <>
@@ -48,19 +77,21 @@ export default function SearchByEVYield() {
             value={selectedStat}
           />
 
-          {/* Reserved for later use */}
-          <div className="grow" />
+          <Select
+            className="grow"
+            label="Search in Pokédex"
+            options={pokedexOptions}
+            onChange={opt =>
+              setPokedex(opt?.value ? pokedexByCode[opt.value] : null)
+            }
+            value={selectedPokedex}
+          />
         </div>
 
         <div className="mt-8 space-y-3">
           <h2 className="text-left text-2xl font-bold">Pokémon List</h2>
 
-          {pokemonList.length > 0 && (
-            <PokemonList
-              numberCallback={pokemon => pokemon.speciesId}
-              pokemonList={pokemonList}
-            />
-          )}
+          {pokemonList.length > 0 && <PokemonList pokemonList={pokemonList} />}
         </div>
       </div>
     </>
