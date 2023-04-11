5619626acd5d5d1c7d3d2a13a9173ebd267d0e89
Simón Oroño <simonorono@protonmail.com>
2023-04-11 07:24:00 -0400

    Update rdex-data to the latest version and bump data version

--- src/api/cache.ts
+++ src/api/cache.ts
@@ -4,7 +4,7 @@ import { name } from '../../package.json'
 /**
  * Bump this number every time the data gets updated to invalidate the cache.
  */
-const DATA_VERSION = 27
+const DATA_VERSION = 28
 
 localforage.config({
   name: `${name}@DATA-${DATA_VERSION}`,
