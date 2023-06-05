2da8c1a94ca2f40f93e8212bcd38c775728af861
Simón Oroño <simonorono@protonmail.com>
2023-06-05 07:58:37 -0400

    Bump data version

--- src/api/cache.ts
+++ src/api/cache.ts
@@ -4,7 +4,7 @@ import { name } from '../../package.json'
 /**
  * Bump this number every time the data gets updated to invalidate the cache.
  */
-const DATA_VERSION = 29
+const DATA_VERSION = 30
 
 localforage.config({
   name: `${name}@DATA-${DATA_VERSION}`,
