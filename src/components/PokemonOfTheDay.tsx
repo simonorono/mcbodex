159618d4344fc375bb4f6a3e16731aa613c2e9f9
Simón Oroño <simonorono@protonmail.com>
2023-06-24 08:20:43 -0400

    Make the Pokémon of the day use the vertical card

--- src/components/PokemonOfTheDay.tsx
+++ src/components/PokemonOfTheDay.tsx
@@ -14,6 +14,7 @@ export default function PokemonOfTheDay() {
           <PokemonCard
             pokemon={pokemonOfTheDay}
             number={pokemonOfTheDay.speciesId}
+            vertical
           />
         </div>
       )}
