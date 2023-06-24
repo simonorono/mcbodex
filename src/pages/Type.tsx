e7dff3205912a4b0ebeef6ef10bc1bd24d649288
Simón Oroño <simonorono@protonmail.com>
2023-06-24 08:21:01 -0400

    Adjust responsive design on type effectiveness display

--- src/pages/Type.tsx
+++ src/pages/Type.tsx
@@ -9,7 +9,7 @@ import { title, types } from '../utils'
 
 function renderEffectiveness(data: { [id: number]: number }) {
   return (
-    <div className="grid grid-cols-3 gap-2 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-2 gap-2 lg:grid-cols-3">
       {types.all.map(type => (
         <TypeDamageBadge key={type.id} type={type} effect={data[type.id]} />
       ))}
