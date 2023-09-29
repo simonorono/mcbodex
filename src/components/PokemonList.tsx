963a4a84035c49f5bc11a35a2421cd3cb25c8067
Simón Oroño <simonorono@protonmail.com>
2023-09-29 07:46:14 -0400

    Remove load on scroll from Pokémon list

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { useInView } from 'react-intersection-observer'
 import Loader from './Loader'
 import PokemonCard from './PokemonCard'
 
@@ -8,26 +7,11 @@ interface Props {
   numberCallback?: (pokemon: Pokemon) => number
 }
 
-const INITIAL_PAGE = 1
-const PER_PAGE = 60
-
 export default function PokemonList({ pokemonList, numberCallback }: Props) {
   const [loaded, setLoaded] = useState(false)
-  const [currentPage, setCurrentPage] = useState(INITIAL_PAGE)
 
-  const allPagesLoaded = currentPage * PER_PAGE > pokemonList.length
-
-  const { ref: scrollDetector } = useInView({
-    onChange: inView => {
-      if (inView && !allPagesLoaded) {
-        setCurrentPage(currentPage + 1)
-      }
-    },
-  })
-
-  useEffect(() => setLoaded(true), [])
-
-  useEffect(() => setCurrentPage(INITIAL_PAGE), [pokemonList])
+  useEffect(() => setLoaded(false), [pokemonList])
+  useEffect(() => setLoaded(true))
 
   return (
     <>
@@ -37,7 +21,6 @@ export default function PokemonList({ pokemonList, numberCallback }: Props) {
         <>
           <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
             {pokemonList
-              .slice(0, currentPage * PER_PAGE)
               .map((pokemon: Pokemon, index) => (
                 <li key={pokemon.id} className="col-span-1">
                   <PokemonCard
@@ -49,11 +32,6 @@ export default function PokemonList({ pokemonList, numberCallback }: Props) {
                 </li>
               ))}
           </ul>
-
-          <div
-            ref={scrollDetector}
-            className="relative -top-96 -z-50 border-2 border-black"
-          />
         </>
       )}
     </>
